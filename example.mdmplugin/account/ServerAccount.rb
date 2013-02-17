4b45c5e66339d05e972b379b0df33414ebc6d72b
Alberto Lagos <alberto.lagos@gmail.com>
2013-02-17 20:52:31 -0300

    Added lines to test curl library

--- example.mdmplugin/account/ServerAccount.rb
+++ example.mdmplugin/account/ServerAccount.rb
@@ -6,6 +6,23 @@ class ServerAccount
 	end
 
 	def on_login 
-		"Esto deberia funcionar para cargar login ;D, con usuario: #{@username} y #{@password} - #{self}"
+		#val = JSON::parse('{"verdadero":"bar"}')
+		@hashier = Hash.new
+		@hashier["error"] = false
+		#@hashier["message"] = "Esto deberia funcionar para cargar login ;D, con usuario: #{@username} y #{@password} - #{self} - #{val} && #{val["verdadero"]} - keys: #{val.keys}"
+		@hashier[:cookies] = "some cookies here! ;D"
+		@hashier["message"] = do_something
+		
+		@hashier
+	end
+
+	def do_something
+		ret = ""
+		#url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=mruby&rsz=large"
+		#for result in JSON::parse(Curl::get(url).body)['responseData']['results']
+		#	ret += "#{result['titleNoFormatting']}\n  #{result['url']}"
+		#end
+		ret = Curl::get("http://www.ruby-lang.org/ja/", {"User-Agent"=>"curl"}).body
+		ret
 	end
 end
